40e93885cd8a8394b6638e321a05f0d0d55dfb32
Marcos Juarez <marcosjuarez@utexas.edu>
2021-03-07 17:57:23 -0600

    Added tilemap to level select menu

--- levelSelect.js
+++ levelSelect.js
@@ -2,9 +2,19 @@
 demo.levelSelect = function(){};
 demo.levelSelect.prototype = {
     preload: function(){
+        game.load.tilemap('map', 'assets/map/open_world_map.json',null,Phaser.Tilemap.TILED_JSON);
+        game.load.image('map_sky_night2', 'assets/map/map_sky_night2.png');
+        game.load.image('map_sky_planets', 'assets/map/map_sky_planets.png');
     },
     create: function(){
         addChangeStateEventListeners();
+        
+        map = game.add.tilemap('map');
+        map.addTilesetImage('map_sky_night2');
+        map.addTilesetImage('map_sky_planets');
+        
+        sky = map.createLayer("sky");
+        planets = map.createLayer("planets");
         
     },
     update: function(){}
