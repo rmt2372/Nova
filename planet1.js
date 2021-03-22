b227702713b825a839d2d60c13744cc021589dfa
Marcos Juarez <marcosjuarez@utexas.edu>
2021-03-22 12:25:06 -0500

    added level 1 tilemap

--- planet1.js
+++ planet1.js
@@ -7,9 +7,22 @@ demo.planet1.prototype = {
         game.load.image('laser', 'assets/sprites/Laser_cannon.png');
         game.load.image('missle', 'assets/sprites/Smart_missle.png');
         game.load.image('burst', 'assets/sprites/Debris_burst.png');
+        
+        game.load.tilemap('map', 'assets/map/level_1_map.json',null,Phaser.Tilemap.TILED_JSON);
+        game.load.image('map_sky_night2', 'assets/map/map_sky_night2.png');
+        game.load.image('level_1_Ground', 'assets/map/level_1_Ground.png');
     },
     create: function(){
         addChangeStateEventListeners();
+        
+        map = game.add.tilemap('map');
+        map.addTilesetImage('map_sky_night2');
+        map.addTilesetImage('level_1_Ground');
+        
+        sky = map.createLayer("sky");
+        planets = map.createLayer("ground");
+        
+        map.setCollisionBetween(13,265,true,"ground");
     },
     update:function(){}
 }
