7a8574ae2f43104c2f1e2462c8e8f064ba9804e7
Marcos Juarez <marcosjuarez@utexas.edu>
2021-04-04 20:06:11 -0500

    added Nova to level 2 map

--- planet2.js
+++ planet2.js
@@ -2,6 +2,10 @@
 demo.planet2 = function(){};
 demo.planet2.prototype = {
     preload: function(){
+        
+        game.load.spritesheet('nova', 'assets/sprites/nova_.png', 91, 110);
+        
+        
         game.load.tilemap("map", "assets/map/level_2_map.json",null,Phaser.Tilemap.TILED_JSON);
         game.load.image("map_sky_night2","assets/map/map_sky_night2.png");
         game.load.image("level_2_ground","assets/map/level_2_Ground.png");
@@ -19,6 +23,15 @@ demo.planet2.prototype = {
         ground = map.createLayer("ground");
         
         sky.resizeWorld();
+        
+        map.setCollisionBetween(5,20,true,"ground");
+        
+        nova = game.add.sprite(31, 1017, 'nova');
+        
+        
+        game.camera.follow(nova);
+        
+        
         
     },
     update:function(){}
