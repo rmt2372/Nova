6ccd9127b5858997bdf8715bf36b7a5d9dcbccdd
dylanngo44 <dylanngo44@gmail.com>
2021-04-08 22:36:59 -0500

    fixed itch.io bugs

--- planet3.js
+++ planet3.js
@@ -4,7 +4,7 @@ demo.planet3.prototype = {
     preload: function(){
         game.load.tilemap('map', 'assets/map/level_3_map.json',null,Phaser.Tilemap.TILED_JSON);
         game.load.image('map_sky_night2', 'assets/map/map_sky_night2.png');
-        game.load.image('Level_3_Ground', 'assets/map/level_3_Ground.png');
+        game.load.image('Level_3_Ground', 'assets/map/Level_3_Ground.png');
         game.load.image('shot', 'assets/sprites/Nova_shot.png');
         game.load.spritesheet('nova', 'assets/sprites/nova_.png', 91, 110);
     },
